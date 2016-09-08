56e034094b9227c826484f1014d85d727b168fab
IlyaFinkelshteyn <ilyaf@appveyor.com>
2016-09-07 19:42:11 -0700

    Initial commit

---
+++ UnitTestsAppveyorTests/Class1Tests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestsAppveyor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestsAppveyor.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void SumTest()
+        {
+            Class1 class1 = new Class1();
+            Assert.IsTrue(class1.Sum(1, 2) == 3);
+        }
+    }
+}
