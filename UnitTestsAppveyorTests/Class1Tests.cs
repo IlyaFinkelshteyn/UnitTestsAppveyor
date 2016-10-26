772778c1fdbb3d811cb420e1c6df1aba8730ded9
IlyaFinkelshteyn <ilyaf@appveyor.com>
2016-10-26 13:01:36 -0700

    Traces

--- UnitTestsAppveyorTests/Class1Tests.cs
+++ UnitTestsAppveyorTests/Class1Tests.cs
@@ -6,29 +6,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Diagnostics;
 
 namespace UnitTestsAppveyor.Tests
 {
     [TestClass()]
     public class Class1Tests
     {
+
+        //public Class1Tests()
+        //{
+        //    TraceListener[] listeners = {
+        //        new TextWriterTraceListener(Console.Out)
+        //    };
+        //    Debug.Listeners.AddRange(listeners);
+        //}
+
         [AssemblyInitialize()]
         public static void AssemblyInit(TestContext context)
         {
-            Console.WriteLine("AssemblyInit " + context.TestName);
-            Thread.Sleep(TimeSpan.FromMinutes(1));
+           // Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+            //Trace.WriteLine("AssemblyInit 1 " + context.TestName);
+            //Trace.Flush();
+            //Thread.Sleep(1);
+            Trace.WriteLine("AssemblyInit 2 " + context.TestName);
+            //Trace.Flush();
         }
+
+
         [TestMethod()]
         public void SumTest()
+        {
+            Trace.Flush();
+            Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+            Trace.WriteLine("Sum test 123");
+            Trace.Flush();
+            Class1 class1 = new Class1();
+            Assert.IsTrue(class1.Sum(1, 2) == 3);
+        }
+
+        [TestMethod()]
+        public void Sum2()
         {
             Class1 class1 = new Class1();
             Assert.IsTrue(class1.Sum(1, 2) == 3);
         }
-        
-        [TestMethod()]
-        public void BrokenTest()
-        {
-            Assert.IsTrue(1 == 2);
-        }
     }
 }
+
