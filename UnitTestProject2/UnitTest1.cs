37cea999cefe83aa4deb1663fac32032a829dfa6
IlyaFinkelshteyn <ilyaf@appveyor.com>
2016-11-02 14:29:37 -0700

    better config for 2 test projects

--- UnitTestProject2/UnitTest1.cs
+++ UnitTestProject2/UnitTest1.cs
@@ -1,15 +1,32 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestsAppveyor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
 
-namespace UnitTestProject2
+namespace UnitTestsAppveyor.Tests
 {
-    [TestClass]
-    public class UnitTest1
+    [TestClass()]
+    public class Class1Tests
     {
-        [TestMethod]
-        public void TestMethod1()
+
+        [TestMethod()]
+        public void SumTest22()
         {
-            Assert.AreEqual(1, 1);
+            Class1 class1 = new Class1();
+            Assert.IsTrue(class1.Sum(1, 2) == 3);
+        }
+
+        [TestMethod()]
+        public void Sum33()
+        {
+            Class1 class1 = new Class1();
+            Assert.IsTrue(class1.Sum(1, 2) == 3);
         }
     }
 }
+
